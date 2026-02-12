f78f0690bf28e66f6bf3f2ad5783b89728071522
Sinisa <sinisha.stojanovic@gmail.com>
2026-02-12 21:17:24 +0100

    Root page background image added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Section from "@/components/layout/Section";
 export default function Home() {
   return (
     <>
-      <Section childClassName="bg-gray-300 p-8 rounded-lg">
+      <div
+        className={
+          "absolute inset-0 bg-[url(/images/banner1-img.png)] bg-cover bg-center"
+        }
+      ></div>
+      <Section childClassName="bg-gray-300 p-8 rounded-lg relative z-10">
         <main>
           <h1 className="text-4xl font-bold mb-4">MAIN PAGE</h1>
           <p className="text-lg mb-4">
