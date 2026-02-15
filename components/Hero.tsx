21083036f813c2f8041fce600018f4e46e719e7f
Sinisa <sinisha.stojanovic@gmail.com>
2026-02-15 22:43:32 +0100

    Hero section responsiveness fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,15 +38,13 @@ const Hero = ({
   className,
 }: HeroProps) => {
   return (
-    <Section
-      sectionClassName={cn("pb-[30px] lg:pb-32 relative z-20", className)}
-    >
-      <div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12">
-        <div className="flex flex-col items-center gap-5 text-center md:items-start md:text-left ml-18 lg:ml-0">
-          <h1 className="text-[70px]/[70px] text-black font-secondary font-semibolds text-pretty -tracking-[0.1px] text-center lg:text-left lg:text-6xl mt-7.5 lg:pt-40 mb-6.5">
+    <Section sectionClassName={cn("pb-[30px] relative z-20", className)}>
+      <div className="grid items-center gap-6 md:grid-cols-2 lg:gap-8 lg:mt-20 2xl:mt-44">
+        <div className="flex flex-col items-center text-center md:items-start md:text-left sm:mb-8 lg:ml-18 xl:ml-20">
+          <h1 className="text-[34px]/[38px] sm:text-[42px]/[44px] md:text-[46px]/[52px] lg:text-[62px]/[68px] xl:text-[70px]/[70px] text-black font-secondary  text-pretty -tracking-[0.1px] text-center md:text-left mt-7.5 mb-3.75 lg:mb-6.5">
             {heading}
           </h1>
-          <p className="max-w-xl md:text-[20px]/[34px] text-center md:text-left mx-auto md:mx-0 text-gray-primary mb-7.75">
+          <p className="max-w-[95%] sm:max-w-[75%] md:max-w-[90%] lg:max-w-[95%] text-[16px]/[26px] sm:text-[18px]/[30px] md:text-[18px]/[30px] lg:text-[20px]/[34px] text-center md:text-left mx-auto md:mx-0 text-gray-primary mb-6 lg:mb-7.75">
             {description}
           </p>
           <div className="flex w-full flex-col items-center md:items-start justify-center gap-2 md:justify-start">
@@ -65,7 +63,7 @@ const Hero = ({
           </div>
         </div>
         <div className="relative w-full h-full">
-          <figure className="relative mx-auto lg:absolute lg:top-13.25 lg:left-10 w-[60%] lg:w-[95%]">
+          <figure className="relative mx-auto md:absolute md:top-1/2 md:left-1/2 md:-translate-1/2 w-[60%] md:w-full lg:w-[80%]">
             <img
               src={image.src}
               alt={image.alt}
