e35bf04f2d0acbe9b2c9acb7ed9083ce3f5ac358
Sinisa <sinisha.stojanovic@gmail.com>
2026-02-21 18:07:26 +0100

    Contact and FQA routes added

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Section from "@/components/layout/Section";
+
+const ContactPage = () => {
+  return (
+    <Section>
+      <h1>Contact Page</h1>
+    </Section>
+  );
+};
+
+export default ContactPage;
---
+++ app/fqa/page.tsx
@@ -0,0 +1,11 @@
+import Section from "@/components/layout/Section";
+
+const FqaPage = () => {
+  return (
+    <Section>
+      <div>FQA Page</div>
+    </Section>
+  );
+};
+
+export default FqaPage;
