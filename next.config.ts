66a8b09209e16eb9e652029275cd0c7b80a021ba
Sinisa <sinisha.stojanovic@gmail.com>
2026-03-31 23:59:10 +0200

    Automatic upgrade of unsecured requests

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: false,
 
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
+
   async redirects() {
     return [
       { source: "/about", destination: "/uber-uns", permanent: true },
