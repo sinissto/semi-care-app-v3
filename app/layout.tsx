dfa476d52e0154c0f7d6a2eb954e6d5c4cac43b7
Sinisa <sinisha.stojanovic@gmail.com>
2026-05-27 13:27:10 +0200

    Google Analytics implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import type { Metadata } from "next";
 import { Open_Sans, DM_Serif_Display } from "next/font/google";
 import "./globals.css";
@@ -158,6 +159,21 @@ export default function RootLayout({
           <main className={""}>{children}</main>
         </AppContextProvider>
         <Footer />
+
+        {/* GOOGLE ANALYTICS */}
+        <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       </body>
     </html>
   );
